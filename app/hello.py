f913a4bb06f99c82eed9f3f6ad753874f6eaa949
Oblivion <maxqingx@163.com>
2021-06-01 17:52:50 +0800

    Display sleep time

--- app/hello.py
+++ app/hello.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    sleep_time = 1
     uname = platform.uname()
-    time.sleep(1)
-    return "<p>Hello, World!</p>" + f"Node Name: {uname.node}"
+    time.sleep(sleep_time)
+    return "<p>Hello, World!</p>" + f"Node Name: {uname.node}" + f"Sleep time: {sleep_time}"
