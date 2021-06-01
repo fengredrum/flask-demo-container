c47491b8114abe9644d661814a08202a56014c66
Oblivion <maxqingx@163.com>
2021-06-01 17:47:25 +0800

    Add sleep func

--- app/hello.py
+++ app/hello.py
@@ -1,4 +1,5 @@
 import platform
+import time
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     uname = platform.uname()
+    time.sleep(1)
     return "<p>Hello, World!</p>" + f"Node Name: {uname.node}"
